d1bcfb7f6302215c988a63e358f13ee714a784e6
AdShEaTaKoRe <tomy.berman1@gmail.com>
2020-02-13 13:02:19 +0000

    fixed minor issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,9 @@ class UsersController < ApplicationController
     
       def create
         @user = User.create(user_params)
-        # if @user.valid?
-        #   session[:user_id] = @user.id
-          render json: @user, status: :created, location: @user
-        # else
-        # render json: @user.errors, status: :unprocessable_entity          
-        # end
+
+          render json: @user
+
       end
 
     private
